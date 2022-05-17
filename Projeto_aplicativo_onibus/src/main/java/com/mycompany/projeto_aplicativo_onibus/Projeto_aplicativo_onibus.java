15a96c09087dd59f27edf3c105aa887fe749ce61
gildasio <gildasio@Gildasio-Linux>
2022-05-16 22:51:56 -0400

    Criacao classe main

---
+++ Projeto_aplicativo_onibus/src/main/java/com/mycompany/projeto_aplicativo_onibus/Projeto_aplicativo_onibus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projeto_aplicativo_onibus;
+
+/**
+ *
+ * @author gildasio
+ */
+public class Projeto_aplicativo_onibus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
